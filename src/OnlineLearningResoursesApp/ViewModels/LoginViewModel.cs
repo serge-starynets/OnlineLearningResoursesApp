a9a0f94ef2da975b7894fde48a433bbea1a9a730
Serge Starynets <svsd@bigmir.net>
2017-03-03 23:01:54 +0200

    ..

--- src/OnlineLearningResoursesApp/ViewModels/LoginViewModel.cs
+++ src/OnlineLearningResoursesApp/ViewModels/LoginViewModel.cs
@@ -9,9 +9,12 @@ namespace OnlineLearningResourcesApp.ViewModels
     public class LoginViewModel
     {
         [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Username { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string Password{ get; set; }
     }
 }
--- src/OnlineLearningResoursesApp/ViewModels/RegisterViewModel.cs
+++ src/OnlineLearningResoursesApp/ViewModels/RegisterViewModel.cs
@@ -16,6 +16,7 @@ namespace OnlineLearningResourcesApp.ViewModels
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
